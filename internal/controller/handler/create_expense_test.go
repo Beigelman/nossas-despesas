3aa52db74b1b5956b6d3e7f7d1f4b284445080d4
Beigelman <daniel.beigelman@tino.com.br>
2023-10-14 12:57:37 -0300

    add mock files

--- internal/controller/handler/create_expense_test.go
+++ internal/controller/handler/create_expense_test.go
@@ -1,16 +1,10 @@
 package handler_test
 
-import (
-	"context"
-	"github.com/Beigelman/ludaapi/internal/domain/entity"
-	"github.com/Beigelman/ludaapi/internal/usecase"
-	"testing"
-)
-
-func TestCreateExpenseHandler(t *testing.T) {
-
-	createExpense := func(ctx context.Context, p usecase.CreateExpenseParams) (*entity.Expense, error) {
-		return nil, nil
-	}
-
-}
+//func TestCreateExpenseHandler(t *testing.T) {
+//	createExpense := func(ctx context.Context, p usecase.CreateExpenseParams) (*entity.Expense, error) {
+//		return nil, nil
+//	}
+//
+//
+//
+//}
