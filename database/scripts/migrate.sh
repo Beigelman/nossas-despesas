d399c803d66ca01a4369527f4549fcf0109c3b6a
Daniel Beigelman <50420424+Beigelman@users.noreply.github.com>
2024-02-10 20:08:53 -0300

    feat(): add config env bidings (#3)
    Co-authored-by: Beigelman <daniel.beigelman@tino.com.br>

---
+++ database/scripts/migrate.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Check for correct number of arguments
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <up|down> <migration_path>"
+    exit 1
+fi
+
+# Assign arguments to variables
+MIGRATION_DIRECTION=$1
+MIGRATION_PATH=$2
+
+# Validate migration direction
+if [ "$MIGRATION_DIRECTION" != "up" ] && [ "$MIGRATION_DIRECTION" != "down" ]; then
+    echo "Invalid migration direction: $MIGRATION_DIRECTION. Please specify 'up' or 'down'."
+    exit 1
+fi
+
+# Validate migration path
+if [ ! -d "$MIGRATION_PATH" ]; then
+    echo "Invalid migration path: $MIGRATION_PATH does not exist or is not a directory."
+    exit 1
+fi
+
+# Check if DB_CONNECTION_STRING is set
+if [ -z "${DB_CONNECTION_STRING}" ]; then
+    echo "DB_CONNECTION_STRING is not set. Attempting to construct from components..."
+
+    # Check if required components are set
+    if [ -z "${DB_HOST}" ] || [ -z "${DB_NAME}" ] || [ -z "${DB_PASSWORD}" ] || [ -z "${DB_PORT}" ] || [ -z "${DB_USER}" ]; then
+        echo "One or more required environment variables (DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER) are missing."
+        exit 1
+    else
+        # Construct DB_CONNECTION_STRING
+        export DB_CONNECTION_STRING="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+        echo "Constructed DB_CONNECTION_STRING: ${DB_CONNECTION_STRING}"
+    fi
+else
+    echo "Using existing DB_CONNECTION_STRING."
+fi
+
+# Run migrations using go-migrate in the specified direction and path
+migrate -path "$MIGRATION_PATH" -database "${DB_CONNECTION_STRING}" $MIGRATION_DIRECTION
+
+# Check if migrate succeeded
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+    echo "Migrations $MIGRATION_DIRECTION applied successfully in path $MIGRATION_PATH."
+else
+    echo "Failed to apply migrations $MIGRATION_DIRECTION in path $MIGRATION_PATH."
+    exit 1
+fi
---
+++ internal/config/constructor.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"time"
+
+	"github.com/Beigelman/ludaapi/internal/pkg/env"
+)
+
+type Db struct {
+	Host             string        `mapstructure:"DB_HOST"`
+	Port             string        `mapstructure:"DB_PORT"`
+	Name             string        `mapstructure:"DB_NAME"`
+	User             string        `mapstructure:"DB_USER"`
+	Password         string        `mapstructure:"DB_PASSWORD"`
+	Type             string        `mapstructure:"DB_TYPE"`
+	ConnectionString string        `mapstructure:"DB_CONNECTION_STRING"`
+	MigrationPath    string        `mapstructure:"DB_MIGRATION_PATH"`
+	MaxIdleConns     int           `mapstructure:"DB_MAX_IDLE_CONNS"`
+	MaxOpenConns     int           `mapstructure:"DB_MAX_OPEN_CONNS"`
+	MaxLifeTime      time.Duration `mapstructure:"DB_MAX_LIFE_TIME"`
+	MaxIdleTime      time.Duration `mapstructure:"DB_MAX_IDLE_TIME"`
+}
+
+type Config struct {
+	loader      *viper.Viper
+	Env         env.Environment `mapstructure:"ENV"`
+	ServiceName string          `mapstructure:"SERVICE_NAME"`
+	Port        string          `mapstructure:"PORT"`
+	LogLevel    string          `mapstructure:"LOG_LEVEL"`
+	JWTSecret   string          `mapstructure:"JWT_SECRET"`
+	Db          Db              `mapstructure:",squash"`
+}
+
+func New(env env.Environment) Config {
+	return Config{
+		loader: viper.New(),
+		Env:    env,
+		Port:   "8080",
+		Db: Db{
+			Type:         "postgres",
+			MaxOpenConns: 4,
+		},
+	}
+}
+
+func NewTestConfig(dbPort, dbHost, dbType string) Config {
+	return Config{
+		loader:      viper.New(),
+		ServiceName: "test-luda-api",
+		Env:         "test",
+		Port:        "8080",
+		Db: Db{
+			Host:          dbHost,
+			Port:          dbPort,
+			Name:          "test",
+			User:          "root",
+			Password:      "root",
+			Type:          dbType,
+			MigrationPath: "file:///Users/danielbeigelman/mydev/go-luda/server/database/migrations",
+			MaxOpenConns:  4,
+		},
+	}
+}
