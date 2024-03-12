609f7abe2c84eab8da21ab2e76c42fef62299753
Beigelman <daniel.beigelman@tino.com.br>
2024-03-12 14:54:02 -0300

    add mail api key to cloud run

--- internal/boot/clients.go
+++ internal/boot/clients.go
@@ -3,6 +3,8 @@ package boot
 import (
 	"context"
 	"fmt"
+	"log/slog"
+
 	"github.com/Beigelman/nossas-despesas/internal/config"
 	"github.com/Beigelman/nossas-despesas/internal/domain/service"
 	"github.com/Beigelman/nossas-despesas/internal/infra/email"
@@ -11,7 +13,6 @@ import (
 	"github.com/Beigelman/nossas-despesas/internal/pkg/di"
 	"github.com/Beigelman/nossas-despesas/internal/pkg/env"
 	"github.com/Beigelman/nossas-despesas/internal/pkg/eon"
-	"log/slog"
 )
 
 var ClientsModule = eon.NewModule("Clients", func(ctx context.Context, c *di.Container, lc eon.LifeCycleManager, info eon.Info) {
@@ -25,11 +26,11 @@ var ClientsModule = eon.NewModule("Clients", func(ctx context.Context, c *di.Con
 		return dbClient
 	})
 	di.Provide(c, func(cfg *config.Config) service.EmailProvider {
-		if cfg.Env == env.Production {
-			return email.NewResendEmailProvider(cfg.Mail.ApiKey)
+		if cfg.Env == env.Development {
+			return email.NewMailTrapEmailProvider(cfg.Mail.ApiKey)
 		}
 
-		return email.NewMailTrapEmailProvider(cfg.Mail.ApiKey)
+		return email.NewResendEmailProvider(cfg.Mail.ApiKey)
 	})
 
 	lc.OnDisposing(eon.HookOrders.PREPEND, func() error {
